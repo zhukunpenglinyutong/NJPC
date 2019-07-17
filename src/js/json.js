8ddae45f2904c931f031815c7cf8b3f1296fe7ea
zhukunpenglinyutong <270750933@qq.com>
2019-07-17 17:57:14 +0800

    第7次提交（17:56）| 构建基础的项目展示效果

---
+++ src/js/json.js
@@ -0,0 +1,72 @@
+// tree 数据
+let treeData = [
+    {
+        label: 'HTML',
+        id: 'html',
+        children: [
+            { label: '1.HTML绪论', id: 'html_1' },
+            { label: '2.HTML基础', id: 'html_2' },
+            { label: '3.HTML5', id: 'html_3' }
+        ]
+    },
+    {
+        label: 'CSS',
+        id: 'css'
+    }
+]
+
+// 内容展示 数据
+let mainData = [
+    {
+        title: '集合一',
+        projects: [
+            [
+                {
+                    url: 'https://cn.vuejs.org/v2/guide/list.html', // 链接的地址
+                    name: '项目名称一', // 项目名称
+                },
+                {
+                    url: 'https://zhukunpenglinyutong.github.io/', // 链接的地址
+                    name: '项目名称二', // 项目名称
+                },
+                {
+                    url: 'https://ww.iduom', // 链接的地址
+                    name: '项目名称三', // 项目名称
+                }
+            ],
+            [
+                {
+                    url: 'https://www.baidu.com', // 链接的地址
+                    name: '项目名称四', // 项目名称
+                }
+            ]
+        ]
+    },
+    {
+        title: '集合二',
+        projects: [
+            [
+                {
+                    url: 'https://www.baidu.com', // 链接的地址
+                    name: '集合二的 项目一', // 项目名称
+                }
+            ]
+        ]
+    },
+    {
+        title: '集合三',
+        projects: [
+            [
+                {
+                    url: 'https://www.baidu.com', // 链接的地址
+                    name: '集合三的 项目一', // 项目名称
+                },
+                {
+                    url: 'https://www.baidu.com', // 链接的地址
+                    name: '集合三的 项目二', // 项目名称
+                }
+            ]
+        ]
+    }
+]
+
